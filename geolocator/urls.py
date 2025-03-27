664931132d639fd96ea290419a7a90183118f056
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2025-03-27 15:22:01 -0400

    ultimos cambiesitos

--- geolocator/urls.py
+++ geolocator/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from geolocatorapp import views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.upload_csv, name='upload_csv'),
-    path('get_processing_status/', views.get_processing_status, name='get_processing_status'),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
